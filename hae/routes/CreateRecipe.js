fd9437df5ea80f7319d792493b5eb2d6b69bd9b7
Marina <marina.beck@hfg-gmuend.de>
2023-06-12 16:17:50 +0200

    Alle Elemente eingefügt
    ohne Funktion; Versuch, die Anordnung besser zu machen

--- hae/routes/CreateRecipe.js
+++ hae/routes/CreateRecipe.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { NativeBaseProvider, Text, Input, Flex, Select, Center, FormControl, CheckIcon, WarningOutlineIcon } from "native-base";
+import { NativeBaseProvider, Text, Input, Flex, Select, FormControl, CheckIcon, WarningOutlineIcon, Circle, Button } from "native-base";
 
 const CreateRecipe = () => {
     const [recipeTitle, setRecipeTitle] = useState("");
@@ -18,25 +18,118 @@ const CreateRecipe = () => {
                     onChangeText={(text) => setRecipeTitle(text)}
                     value={recipeTitle}
                 />
-                <Text>Rezeptliste:</Text>
-                <Center>
-                    <FormControl w="3/4" maxW="300" isRequired isInvalid>
-                        <FormControl.Label>Choose service</FormControl.Label>
-                        <Select minWidth="200" accessibilityLabel="Choose Service" placeholder="Choose Service" _selectedItem={{
-                            bg: "teal.600",
-                            endIcon: <CheckIcon size={5} />
-                        }} mt="1">
-                            <Select.Item label="UX Research" value="ux" />
-                            <Select.Item label="Web Development" value="web" />
-                            <Select.Item label="Cross Platform Development" value="cross" />
-                            <Select.Item label="UI Designing" value="ui" />
-                            <Select.Item label="Backend Development" value="backend" />
-                        </Select>
-                        <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-                            Please make a selection!
-                        </FormControl.ErrorMessage>
-                    </FormControl>
-                </Center>
+                <Flex mt="2">
+                    <Flex direction="row">
+                        <Flex direction="column">
+                            <Text mt="2">Rezeptliste:</Text>
+                            <Text mt="2">Dauer:</Text>
+                        </Flex>
+
+                        <Flex direction="column">
+                            <FormControl ml="2" w="3/4" maxW="300" isRequired isInvalid>
+                                <Select maxWidth="100%" accessibilityLabel="Rezeptliste wählen" placeholder="Rezeptliste wählen" _selectedItem={{
+                                    bg: "teal.600",
+                                    endIcon: <CheckIcon size={5} />
+                                }} mt="1">
+                                    <Select.Item label="Numero uno" value="1" />
+                                    <Select.Item label="Neue Rezeptliste erstellen" value="0" />
+                                </Select>
+                                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+                                    Bitte eine Auswahl treffen.
+                                </FormControl.ErrorMessage>
+                            </FormControl>
+                            <Input
+                                placeholder="Dauer eingeben"
+                                variant="filled"
+                                size="md"
+                                ml="2"
+                                width="100%"
+                                onChangeText={(text) => setRecipeTitle(text)}
+                                value={recipeTitle}
+                            />
+                        </Flex>
+                    </Flex>
+
+
+
+                    {/* <Flex direction="row">
+                        <Text direction="column" mt="2">Rezeptliste:</Text>
+                        <FormControl direction="column" ml="2" w="3/4" maxW="300" isRequired isInvalid>
+                            <Select maxWidth="100%" accessibilityLabel="Rezeptliste wählen" placeholder="Rezeptliste wählen" _selectedItem={{
+                                bg: "teal.600",
+                                endIcon: <CheckIcon size={5} />
+                            }} mt="1">
+                                <Select.Item label="Numero uno" value="1" />
+                                <Select.Item label="Neue Rezeptliste erstellen" value="0" />
+                            </Select>
+                            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+                                Bitte eine Auswahl treffen.
+                            </FormControl.ErrorMessage>
+                        </FormControl>
+                    </Flex>
+                    <Flex direction="row">
+                        <Text direction="column" mt="2">Dauer:</Text>
+                        <Input
+                            direction="column"
+                            placeholder="Dauer eingeben"
+                            variant="filled"
+                            size="md"
+                            ml="2"
+                            width="85%"
+                            onChangeText={(text) => setRecipeTitle(text)}
+                            value={recipeTitle}
+                        />
+                    </Flex> */}
+                    <Text mt="12">Zutaten</Text>
+                    <Flex direction="row" mt="2">
+                        <Input
+                            direction="column"
+                            placeholder="Menge"
+                            variant="filled"
+                            size="md"
+                            mr="2"
+                            width="20"
+                            onChangeText={(text) => setRecipeTitle(text)}
+                            value={recipeTitle}
+                        />
+                        <Input
+                            direction="column"
+                            placeholder="Zutat"
+                            variant="filled"
+                            size="md"
+                            width="75%"
+                            onChangeText={(text) => setRecipeTitle(text)}
+                            value={recipeTitle}
+                        />
+                    </Flex>
+                    <Flex direction="row" mt="4" justifyContent="center">
+                        <Circle size="40px" bg="lightgrey">+
+                        </Circle>
+                    </Flex>
+
+                    <Text mt="12">Zubereitungsschritte</Text>
+                    <Flex direction="column" mt="2">
+                        <Text mb="1">Schritt 1</Text>
+                        <Flex direction="row">
+                            <Button width="100" height="100" variant="outline">+</Button>
+                            <Input
+                                direction="column"
+                                placeholder="Zutat"
+                                variant="filled"
+                                size="md"
+                                ml="2"
+                                width="69%"
+                                onChangeText={(text) => setRecipeTitle(text)}
+                                value={recipeTitle}
+                            />
+                        </Flex>
+                    </Flex>
+                    <Flex direction="row" mt="4" justifyContent="center">
+                        <Circle size="40px" bg="lightgrey">+
+                        </Circle>
+                    </Flex>
+                    <Button mt="4">Speichern</Button>
+                </Flex>
             </Flex>
         </NativeBaseProvider>
     );
